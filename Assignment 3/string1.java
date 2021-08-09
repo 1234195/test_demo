4d4af44a53536d1049dd45718f56da03440f90f3
1234195 <72432791+1234195@users.noreply.github.com>
2021-08-09 22:02:52 +0530

    String Q1

---
+++ Assignment 3/string1.java
@@ -0,0 +1,13 @@
+package javaFirstProject;
+
+public class string1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello World";
+		System.out.println( "The size of "+ "the String is "+ str.length());
+	           
+
+	}
+
+}

aac47abcf1ea2ad6463275831ef26374ce528e6c
1234195 <72432791+1234195@users.noreply.github.com>
2021-08-09 21:39:03 +0530

    Add files via upload
    Assignment-Q2

---
+++ Basic Assignment1/Assignment2.java
@@ -0,0 +1,34 @@
+package javaFirstProject;
+
+public class Assignment2 {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=100,arm;
+		System.out.println("Armstrong numbers between 100 to 999");
+		while(i<1000)
+		{
+		arm=armstrongOrNot(i);
+		if(arm==i)
+		System.out.println(i);
+		i++;
+		}
+		
+	}
+	static int armstrongOrNot(int num)
+	{
+		int x,a=0;
+		while(num!=0)
+		{
+			x=num%10;
+			a=a+(x*x*x);
+			num/=10 ;
+		}
+		return a;
+	}
+	
+
+}

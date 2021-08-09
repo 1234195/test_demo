aa5ce15b1b307a66b0bfcfd83e05ba7cde4ad88c
1234195 <72432791+1234195@users.noreply.github.com>
2021-08-09 22:03:29 +0530

    String Q2

---
+++ Assignment 3/String2.java
@@ -0,0 +1,15 @@
+package javaFirstProject;
+
+
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "Hello,";
+        s = s.concat("How are you?");
+        System.out.println(s);
+
+}
+}

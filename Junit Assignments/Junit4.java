88bc7d3c29a6ba728541cdc015c3b7a4f80d8446
1234195 <72432791+1234195@users.noreply.github.com>
2021-08-14 22:55:36 +0530

    Junit Assignments
    Q4

---
+++ Junit Assignments/Junit4.java
@@ -0,0 +1,21 @@
+
+public class Junit4 {
+
+
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	
+	public int divide(int x,int y) 
+	{
+		return x/y;
+	}
+	
+	public Double AreaC(Double r)
+	{
+		return Math.PI*r*r;
+	}
+	
+}
+

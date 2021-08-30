521367f097090a35aabf265e3e92ddeb808dc290
1234195 <72432791+1234195@users.noreply.github.com>
2021-08-30 12:04:02 +0530

    Spring Security
    Security

---
+++ Security/securityQ2.que3/src/main/java/anjali/sp/securityQ2/que3/Application.java
@@ -0,0 +1,13 @@
+package anjali.sp.securityQ2.que3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
